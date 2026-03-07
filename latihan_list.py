680b78253bde6a8e2103f7497474d1ae959a7213
Ferian Satria <148834456+ferian-satria@users.noreply.github.com>
2026-03-07 14:42:28 +0700

    Selesai

--- latihan_list.py
+++ latihan_list.py
@@ -1,5 +1,5 @@
-# NAMA  : 
-# KELAS : 
+# NAMA  : Shafeera Renata Ibrahim
+# KELAS : 9B
 # ---------------------------------------------------------
 # LATIHAN: REVIEW LIST PYTHON
 #Diberikan sebuah data acak nilai ujian siswa. Buatlah program yang mengurutkan data tersebut 
@@ -16,3 +16,14 @@
 # ---------------------------------------------------------
 
 # Tulis kodemu di bawah ini:
+data_nilai_asli = [75, 55, 90, 45, 95, 80]
+print("Data nilai asli: ", data_nilai_asli)
+data_nilai_asli.sort(reverse=True)
+print("Data setelah diurutkan (Descending): ", data_nilai_asli)
+tiga_nilai_tertinggi = data_nilai_asli[:3]
+print("Tiga nilai tertinggi (Penerima Beasiswa): ", tiga_nilai_tertinggi)
+daftar_nilai_yang_lulus = []
+for nilai in data_nilai_asli:
+    if nilai >= 60:
+        daftar_nilai_yang_lulus.append(nilai)
+print("Daftar nilai yang lulus: ", daftar_nilai_yang_lulus)
